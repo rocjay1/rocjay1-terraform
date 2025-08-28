e4d1963273e258faf6aea069bb9a3f75a7e6727b
Rocco Davino <jasonroc19@gmail.com>
2025-08-27 20:45:25 -0500

    Being picky with logging messages

--- winlab/start_vms.sh
+++ winlab/start_vms.sh
@@ -5,5 +5,5 @@ RG="rg-winlab-o8mr"
 for VM in DC01 DC02 Server01 Server02 Storage01 Client01; do
     echo "Starting $VM..."
     az vm start --resource-group $RG --name $VM
-    echo "...finished starting $VM!"
+    echo "...Finished starting $VM!"
 done
--- winlab/stop_vms.sh
+++ winlab/stop_vms.sh
@@ -5,5 +5,5 @@ RG="rg-winlab-o8mr"
 for VM in DC01 DC02 Server01 Server02 Storage01 Client01; do
     echo "Stopping $VM..."
     az vm deallocate --resource-group $RG --name $VM
-    echo "...finished stopping $VM!"
+    echo "...Finished stopping $VM!"
 done
