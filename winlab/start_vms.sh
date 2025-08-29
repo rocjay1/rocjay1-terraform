d29b8be551f2adeeeedb098b7f421a251b4d4636
Rocco Davino <jasonroc19@gmail.com>
2025-08-28 19:59:40 -0500

    Remove start_vms.sh

--- winlab/start_vms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-RG="rg-winlab-o8mr"
-
-for VM in DC01 DC02 Server01 Server02 Storage01 Client01; do
-    echo "Starting $VM..."
-    az vm start --resource-group $RG --name $VM
-    echo "...Finished starting $VM!"
-done
